78b7bca0036ddb1279a284b22c6c2786e2df2c77
nurtt0949 <71143068+nurtt0949@users.noreply.github.com>
2020-10-05 12:02:09 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *Prime or not prime checker
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +10,42 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //create a Scanner
+    Scanner input = new Scanner(System.in);
+    //ask user for which number to checker
+    System.out.println("Which number would you like to check?");
     
+    //declare a variable
+    int userNum = input.nextInt();
+    //declare a variable that counts
+    int count = 0;
+    int total = 0;
+    do{
+      count = count + 1;
+      if(userNum % count == 0){
+         total = total + 1;
+        System.out.println(count);
+
+      }
+    }while(count < userNum);
+      //determine if its prime or not prime
+      if(total > 2){
+        System.out.println(userNum + " is not a prime number");
+      }else if(total < 2){
+        System.out.println(userNum + " is a prime number");
+      }
   }
 }
+
+    
+  
+    
+  
+
+
+
+    
+  
+    
+  
+
